fe79eb1947dfb3b7debb7d00b136732def91a4b1
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-18 10:20:11 -0400

    added user-agent headers to mangadex and tcb controllers

--- controllers/tcbscansController.js
+++ controllers/tcbscansController.js
@@ -46,7 +46,12 @@ const getLatestChapter = async (url) => {
   let latestChapter = 0;
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+      },
+    });
     const $ = cheerio.load(response.data);
 
     const latestChapterArr = `${$(
