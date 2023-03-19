a047b10a7d0f5c7294d9cef453870e857ad294eb
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-19 10:55:11 -0400

    added more headers to cubari axios get

--- controllers/cubariController.js
+++ controllers/cubariController.js
@@ -31,6 +31,9 @@ const getLatestChapter = async (url) => {
       headers: {
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9',
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
       },
     });
     const $ = cheerio.load(response.data);
