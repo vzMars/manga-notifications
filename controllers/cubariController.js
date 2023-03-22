8adabf2787c35068ed43f19f508680b6bfb960a8
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-22 15:13:13 -0400

    added accept-encoding to axios headers

--- controllers/cubariController.js
+++ controllers/cubariController.js
@@ -29,11 +29,12 @@ const getLatestChapter = async (url) => {
   try {
     const response = await axios.get(url, {
       headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9',
         Accept:
           'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
       },
     });
     const $ = cheerio.load(response.data);
