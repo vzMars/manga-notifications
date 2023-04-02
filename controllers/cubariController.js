96e1e9d39366851a9a02146239ace6c6f6e01c64
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-02 12:32:34 -0400

    added another header

--- controllers/cubariController.js
+++ controllers/cubariController.js
@@ -29,11 +29,13 @@ const getLatestChapter = async (url) => {
   try {
     const response = await axios.get(url, {
       headers: {
-        Accept:
+        accept:
           'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'User-Agent':
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'sec-ch-ua':
+          'Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111',
+        'user-agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
       },
     });
